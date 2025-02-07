1a2d60373c6cd0122942af8859504f5cb9ae962f
rowan-maynor <rowanmaynor@gmail.com>
2025-02-07 13:19:16 -0500

    feat: Implement test cases for example functions
    Too focused on learning, forgot to atomic commit.

---
+++ src/index.js
@@ -0,0 +1,76 @@
+function capitalize(string){
+  let firstLetter = string[0].toUpperCase();
+  return firstLetter + string.slice(1);
+}
+
+function reverseString(string){
+  let reverse = "";
+  for(let i = string.length - 1; i > -1; i--){
+    reverse += string[i];
+    console.log(reverse);
+  }
+  return reverse;
+}
+
+const calculator = {
+  add: function (value1, value2) {
+    return value1 + value2;
+  },
+  subtract: function (value1, value2) {
+    return value1 - value2;
+  },
+  divide: function (value1, value2) {
+    return value1 / value2;
+  },
+  multiply: function (value1, value2) {
+    return value1 * value2;
+  },
+};
+
+function caesarCipher(string, shift){
+  let charCodes = [];
+  for(let i = 0; i < string.length; i++){
+    let charCode = string.charCodeAt(i);
+    if(charCode >= 97 && charCode <= 122){
+      if(charCode + shift > 122){
+        charCode = charCode + shift - 26;
+      } else {
+        charCode = charCode + shift;
+      }
+    } else if(charCode >= 65 && charCode <= 90) {
+      if(charCode + shift > 90){
+        charCode = charCode + shift - 26;
+      } else {
+        charCode = charCode + shift;
+      }
+    }
+    charCodes.push(charCode);
+  }
+  return String.fromCharCode(...charCodes);
+}
+
+function analyzeArray(array){
+  let total = 0;
+  let minResult = array[0];
+  let maxResult = array[0];
+
+  for(let i = 0; i < array.length; i++){
+    total += array[i];
+    if(array[i] > maxResult){
+      maxResult = array[i];
+    } else if (array[i] < minResult){
+      minResult = array[i];
+    }
+  }
+
+  const results = {
+    average: total/array.length,
+    min: minResult,
+    max: maxResult,
+    length: array.length
+  };
+
+  return results;
+}
+
+export { capitalize, reverseString, calculator, caesarCipher, analyzeArray }
---
+++ tests/index.test.js
@@ -0,0 +1,61 @@
+import { analyzeArray, caesarCipher, calculator, capitalize, reverseString } from "../src/index.js";
+
+test("capitalize: Happy path", ()=> {
+  expect(capitalize("this sentence.")).toBe("This sentence.");
+});
+
+test("capitalize: First char is upper", ()=> {
+  expect(capitalize("This sentence.")).toBe("This sentence.");
+});
+
+test("capitalize: First char is number", ()=> {
+  expect(capitalize("10 people sentence.")).toBe("10 people sentence.");
+});
+
+test("capitalize: First char is special char", ()=> {
+  expect(capitalize("$example -terminal -command")).toBe("$example -terminal -command");
+});
+
+test("reverseString: Happy path", ()=>{
+  expect(reverseString("Example String")).toBe("gnirtS elpmaxE");
+});
+
+test("calculator: Add", () => {
+  expect(calculator.add(10, 2)).toBe(12);
+});
+
+test("calculator: Subtract", () => {
+  expect(calculator.subtract(10, 2)).toBe(8);
+});
+
+test("calculator: Divide", () => {
+  expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test("calculator: Multiply", () => {
+  expect(calculator.multiply(10, 2)).toBe(20);
+});
+
+test("caesar: Happy Path", ()=> {
+  expect(caesarCipher("test", 1)).toBe("uftu");
+});
+
+test("caesar: Maintains capital letters", () => {
+  expect(caesarCipher("tEsT", 1)).toBe("uFtU");
+});
+
+test("caesar: Maintains special letters", () => {
+  expect(caesarCipher("te, st!", 1)).toBe("uf, tu!");
+});
+
+test("caesar: Wraps past z", () => {
+  expect(caesarCipher("zzz", 1)).toBe("aaa");
+});
+
+test("caesar: Wraps past Z", () => {
+  expect(caesarCipher("ZZZ", 1)).toBe("AAA");
+});
+
+test("analyzeArray: Happy Path", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({ average: 4, min: 1, max: 8, length: 6 });
+});
